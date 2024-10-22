dc6375affeed745cd7ce02e7618cd4929ebe00bd
Gabi <gabriela.palma@estudante.ifms.edu.br>
2024-10-22 07:35:29 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,38 +6,60 @@
 import express from 'express'
 import cors from 'cors'
 import mysql from 'mysql2/promise'
-// Criar um objeto do tipo express.
+//Criar um objeto do tipo express.
 const app = express()
 //incluir pra ele receber json
-app.use(express.json()) //Middleware
-//incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZWNDO REQUISIÇÃO PARA A PORTA DO SERVIDOR 
+app.use(express.json())  //Middleware
+//incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
 app.use(cors())
 //ROTAS
-app.get("/produtos", async (req,res)=>{
+app.get("/produtos",async(req,res)=>{
 
     //O que eu tenho que fazer aqui dentro?
-    // OK -> PASSO 1: Criar banco de dados
-    //PASSSO 2: Usar a lib mysql2 para poder conectar com o banco 
+    //OK -> PASSO 1: Criar o banco de dados
+    //PASSO 2: Usar a lib mysql2 para conectar com o banco
     try{
         const conexao = await mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost: "localhost",
-            user: process.env.dbuser?process.env.dbuser:"root",
-            password: process.env.dbpassword?process.env.dbpassword:"",
-            database: process.env.dbname?process.env.dbname:"banco1022b",
-            port: process.env.dbport?parseInt(process.env.dbport):3306
+            host: process.env.dbhost?process.env.dbhost:"mysql-marketplace-ifms-mysql-gabi.l.aivencloud.com",
+            user:process.env.dbuser?process.env.dbuser:"avnadmin",
+            password:process.env.dbpassword?process.env.dbpassword:"AVNS_fQDBVN06Z9-Bj0ztd4Z",
+            database:process.env.dbname?process.env.dbname:"defaultdb",
+            port:process.env.dbport?parseInt(process.env.dbport):26924
         })
-        //PASSO 3:  QUERY -> SELECT * FROM produtos 
-        const [result,fields] =  await conexao.query("SELECT * FROM produtos")
+        //PASSO 3: QUERY  -> SELECT * FROM produtos
+        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
         await conexao.end()
-       //PASSO 4: Colocar os dados do banco no response 
+        //PASSO 4: Colocar os dados do banco no response
         res.send(result)
-}catch(e){
-    res.status(500).send("Erro do servidor")
-}
+    }catch(e){
+        res.status(500).send("Erro do servidor")
+    }
+});
 
-})
+app.get("/usuarios",async(req,res)=>{
 
-//INICAR O SERVIDOR 
+    //O que eu tenho que fazer aqui dentro?
+    //OK -> PASSO 1: Criar o banco de dados
+    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"mysql-marketplace-ifms-mysql-gabi.l.aivencloud.com",
+            user:process.env.dbuser?process.env.dbuser:"avnadmin",
+            password:process.env.dbpassword?process.env.dbpassword:"AVNS_fQDBVN06Z9-Bj0ztd4Z",
+            database:process.env.dbname?process.env.dbname:"defaultdb",
+            port:process.env.dbport?parseInt(process.env.dbport):26924
+        })
+        //PASSO 3: QUERY  -> SELECT * FROM produtos
+        const [result,fields]  = await conexao.query("SELECT * FROM usuarios")
+        await conexao.end()
+        //PASSO 4: Colocar os dados do banco no response
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Erro do servidor")
+    }
+});
+
+//INICIAR O SERVIDOR
 app.listen(8000,()=>{
     console.log("SERVIDOR INICIADO NA PORTA 8000")
 })

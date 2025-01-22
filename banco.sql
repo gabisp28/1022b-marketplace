98436d7f0641a80f4953ef942552870bb54eca62
Gabi <gabriela.palma@estudante.ifms.edu.br>
2025-01-22 08:36:30 -0400

    euuu

--- banco.sql
+++ banco.sql
@@ -1,4 +1,6 @@
-USE defaultdb;
+drop database banco1022b;
+create database banco1022b;
+USE banco1022b;
 CREATE TABLE IF NOT EXISTS produtos(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
@@ -6,4 +8,4 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
-INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'https://m.media-amazon.com/images/I/515ahREcK+L._AC_SX679_.jpg');
